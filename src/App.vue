6efb266dadc3bc51e0d6124f8f24471d3912ece7
AngeloC89 <Angelociulla89@gmail.com>
2024-04-10 15:09:34 +0200

    I created the structure and the components

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+<HeaderComponent/>
+<MainComponent/>
+<FooterComponent/>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+
+
+  export default {
+    name:'App',
+    components: {
+      HeaderComponent,
+      FooterComponent,
+      MainComponent
+    },
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
+
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,51 @@
+<template>
+    <header>
+        <div id="nav">
+            <h1>ciolla</h1>
+            <ul>
+                <li><a href="#">link</a></li>
+                <li><a href="#">link</a></li>
+                <li><a href="#">link</a></li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComponent',
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    header {
+        border: 1px solid red;
+        height: 100px;
+        width: 100%;
+        margin: o auto;
+        display: flex;
+        align-items: center;
+
+        #nav{
+            border: 1px solid red;
+            width: 80%;
+            margin: 0 auto;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+            ul{
+                display: flex;
+
+                li{
+                    list-style: none;
+                    text-decoration: none;
+                    
+                    margin: 20px;
+                }
+            }
+        }
+    }
+</style>
