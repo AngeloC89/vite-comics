f2af983daad6d17d17023a89a170426639d352ae
AngeloC89 <Angelociulla89@gmail.com>
2024-04-10 17:37:59 +0200

    I complete FooterTop with icons and links

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,16 +1,24 @@
 <template>
     <footer>
-    
-
+        <FooterTop />
+        <FooterMid />
+        <FooterBottom />
     </footer>
 </template>
 
 <script>
+import FooterTop from './FooterComponents/FooterTop.vue';
+import FooterMid from './FooterComponents/FooterMid.vue';
+
     export default {
-        
-    }
+        name: 'FooterComponent',
+        components: {
+            FooterTop,
+            FooterMid,
+        }
+    };
 </script>
 
 <style lang="scss" scoped>
-
+    /* Add your footer styles here */
 </style>
---
+++ src/components/FooterComponents/FooterTop.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="footertop p-4">
+    <div class="container ">
+        <ul class="d-flex justify-content-between align-items-center list-unstyled">
+            <li class=""  v-for="(item, index) in MenuFooter" :key="index">
+                <img :src="item.icon" alt="Image">
+                <a class="text-decoration-none text-white mx-3 text-uppercase " href="#">{{item.text}}</a></li>
+        </ul>
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+        name: 'FooterTop',
+        data() {
+            return {
+                MenuFooter: [
+                    {
+                        icon: '/img/buy-comics-digital-comics.png',
+                        text: 'digital comics',
+                        url: ''
+                    },
+                    {
+                        icon: '/img/buy-comics-merchandise.png',
+                        text: 'dc merchandise',
+                        url: ''
+                    },
+                    {
+                        icon: '/img/buy-comics-subscriptions.png',
+                        text: 'subscription',
+                        url: ''
+                    },
+                    {
+                        icon: '/img/buy-comics-shop-locator.png',
+                        text: 'comic shop locator',
+                        url: ''
+                    },
+                    {
+                        icon: '/img/buy-dc-power-visa.svg',
+                        text: 'dc power visa',
+                        url: ''
+
+                    },
+                ]
+
+            }
+        },
+
+    }
+</script>
+
+
+
+
+
+<style lang="scss" scoped>
+@use '/src/assets/styles/partials/variables' as *;
+body{
+    
+}
+.footertop{
+    background-color: $backgroundFooterTop;
+
+    ul{
+        margin-bottom: 0px;
+    }
+    
+}
+
+img{
+    height: 50px;
+}
+
+</style>
