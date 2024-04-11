934ab938d9eb9956eb5141dc6d1a0767fa21d220
AngeloC89 <Angelociulla89@gmail.com>
2024-04-11 15:33:46 +0200

    added card component and impoted on main component

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,18 +1,28 @@
 <template>
     <main>
-        <div class="container text-center py-5">
-            <h5 class="text-white"> --> Content goes here <-- </h5>
+        <div class="container text-center">
+            <h2 class="text-white text-uppercase ">current series</h2>
+            <div class="row">
+                <div class="col-12 col-md-3 col-lg-2" v-for="(item, index) in comics" :key="index" >
+                    <CardComponent :image="item.thumb" :series="item.series" :price="item.price"/>
+                </div>
+            </div>
         </div>
     </main>
 </template>
 
 <script>
-import { comics } from '@/data/store';
+    import CardComponent from './MainComponent/CardComponent.vue';
+    import { comics } from '../data/store';
     export default {
         name: 'MainComponent',
+        components: {
+            CardComponent
+        },
         data() {
             return {
-                
+                comics
+
             }
         },
         mounted() {
@@ -30,5 +40,15 @@ import { comics } from '@/data/store';
 
     main {
         background-color: $backgroundMain;
+
+        h2{
+            background-color: $backgroundFooterTop;
+            padding: 10px 0px;
+            width: 300px;
+            margin-right: auto;
+            transform: translateY(-50%);
+        }
+        
     }
+   
 </style>
--- src/components/MainComponent/CardComponent.vue
+++ src/components/MainComponent/CardComponent.vue
@@ -1,15 +1,36 @@
 <template>
     <div>
+        <div>
+            <img :src="image" class="img-fluid w-100 " :alt="series">
+        </div>
+        <div class="card-body mt-3">
+            <h5 class="text-white">{{ series }}</h5>
+            <span>{{ price }}</span>
+        </div>
 
     </div>
 </template>
 
 <script>
     export default {
-        
+        name: 'CardComponent',
+        props: [
+            'image',
+            'series',
+            'price',
+            'link'
+        ],
+
     }
 </script>
 
+
+
 <style lang="scss" scoped>
+
+img{
+    min-height: 300px;
+}
+
 
 </style>
